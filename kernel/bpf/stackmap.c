7ead9f1bf5971d21a50ab8a3eb7e1ce31a537363
Yuntao Wang <ytcoode@gmail.com>
2022-06-08 21:04:28 +0800

    bpf: Fix excessive memory allocation in stack_map_alloc()
    The 'n_buckets * (value_size + sizeof(struct stack_map_bucket))' part of
    the allocated memory for 'smap' is never used, get rid of it.
    
    Fixes: b936ca643ade ("bpf: rework memlock-based memory accounting for maps")
    Signed-off-by: Yuntao Wang <ytcoode@gmail.com>
    Link: https://lore.kernel.org/bpf/20220407130423.798386-1-ytcoode@gmail.com

--- kernel/bpf/stackmap.c
+++ kernel/bpf/stackmap.c
@@ -121,8 +121,8 @@ static struct bpf_map *stack_map_alloc(union bpf_attr *attr)
 		return ERR_PTR(-E2BIG);
 
 	cost = n_buckets * sizeof(struct stack_map_bucket *) + sizeof(*smap);
-	cost += n_buckets * (value_size + sizeof(struct stack_map_bucket));
-	err = bpf_map_charge_init(&mem, cost);
+	err = bpf_map_charge_init(&mem, cost + n_buckets *
+				  (value_size + sizeof(struct stack_map_bucket)));
 	if (err)
 		return ERR_PTR(err);
 
